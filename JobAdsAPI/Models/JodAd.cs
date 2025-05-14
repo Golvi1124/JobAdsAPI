01ce87467990d8382b7db8a63d97536a9b698b99
Golvi1124 <golvi1124@gmail.com>
2025-05-14 12:17:27 +0200

    Add project files.

---
+++ JobAdsAPI/Models/JodAd.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace JobAdsAPI.Models;
+
+public class JodAd
+{
+    public int Id { get; set; }
+    public DateTime PublishedAt { get; set; } = DateTime.UtcNow;
+    public string? CompanyName { get; set; }
+    public string? JobTitle { get; set; }
+    public string? Location { get; set; }
+    public string? JobRole { get; set; } // Frontend, Backend, Fullstack
+    public string? WorkType { get; set; } // Remote, Hybrid, Onsite
+    public string? ExpierienceLevel { get; set; } // Junior, Mid, Senior
+    public bool IsCSharpMentioned { get; set; } = false;
+    public bool IsDotNetMentioned { get; set; } = false;
+    public bool IsSQLMentioned { get; set; } = false;
+    public string? OtherSkills { get; set; } // Other required skills/languages
+}
